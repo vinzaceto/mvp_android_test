e055d6c62d565d7a2653b67b3e25ade5af0dab07
Corrado Canino <corrado.canino@nttdata.com>
2019-02-21 17:25:14 +0100

    init MVP

---
+++ app/src/main/java/com/vinzaceto/mvptest/weather/WeatherMVP.kt
@@ -0,0 +1,30 @@
+package com.vinzaceto.mvptest.weather
+
+public interface WeatherMVP {
+
+    interface View
+    {
+        fun showLoader()
+        fun hideLoader()
+        fun setWeatherData(data: String)
+    }
+
+    interface ViewPresenter
+    {
+        fun requestDataFromServer()
+        fun onDestroy()
+    }
+
+    interface ModelPresenter: Model.OnGetWeatherDataFinishedListener
+
+    interface Model
+    {
+        interface OnGetWeatherDataFinishedListener
+        {
+            fun onFinished(data : String)
+            fun onError(t : Throwable)
+        }
+
+        fun getWeatherData(listener: OnGetWeatherDataFinishedListener)
+    }
+}
---
+++ app/src/main/java/com/vinzaceto/mvptest/weather/WeatherObject.kt
@@ -0,0 +1,6 @@
+package com.vinzaceto.mvptest.weather
+
+class WeatherObject {
+    lateinit var field1: String
+    lateinit var field2: String
+}
