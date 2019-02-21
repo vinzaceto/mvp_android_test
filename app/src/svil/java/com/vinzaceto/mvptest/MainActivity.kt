8bc64718a5acbbf66a4960a8dbe9dd64eb979cce
vincenzo aceto <vincenzo.aceto-apple@unina.it>
2019-02-21 15:42:04 +0100

    added flavors

---
+++ app/src/svil/java/com/vinzaceto/mvptest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.vinzaceto.mvptest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("TAG", "BASE_URL: "+ BuildConfig.BASE_URL)
+    }
+}
